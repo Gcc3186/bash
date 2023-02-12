cd7b55724489cae450aabcab249573cfd86bc181
Gustavo <57454176+Gcc3186@users.noreply.github.com>
2023-02-12 03:26:50 -0700

    Create check_cpu_usage_last_minute.sh

---
+++ check_cpu_usage_last_minute.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Function to get the average CPU usage over the last minute
+get_cpu_usage() {
+  cpu_usage=$(top -bn2 | awk '/Cpu\(s\)/ { cpu = $2 } END { print cpu }')
+  echo "Average CPU Usage over the last minute: $cpu_usage%"
+}
+
+# Function to get the average memory usage over the last minute
+get_memory_usage() {
+  memory_usage=$(free -m | awk '/Mem/ { printf("%.2f%"), $3/$2*100 }')
+  echo "Average Memory Usage over the last minute: $memory_usage"
+}
+
+# Function to get the average disk usage over the last minute
+get_disk_usage() {
+  disk_usage=$(df -h | awk '/\/$/ { printf("%s"), $5 }')
+  echo "Average Disk Usage over the last minute: $disk_usage"
+}
+
+# Call the functions to get the usage information
+echo "--- System Usage ---"
+get_cpu_usage
+get_memory_usage
+get_disk_usage
